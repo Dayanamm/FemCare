8ba7c75ca195633f80dbc0ed9d33a406ea13468d
Paola Sarmiento <paolasarmiento@pop-os.localdomain>
2020-08-22 14:40:12 -0500

    Updates

---
+++ precache-manifest.af4b8a398c92b15985f82bbd181d1c5a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c955a470145411f10dd11c3b2bb6503d",
+    "url": "/femcare/index.html"
+  },
+  {
+    "revision": "31a831555f5f5e254386",
+    "url": "/femcare/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b8403c55d022e90bc34b",
+    "url": "/femcare/static/css/main.4ac92789.chunk.css"
+  },
+  {
+    "revision": "31a831555f5f5e254386",
+    "url": "/femcare/static/js/2.d619b0eb.chunk.js"
+  },
+  {
+    "revision": "bc3d92b8a267d342eb7718b9a67f62b8",
+    "url": "/femcare/static/js/2.d619b0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8403c55d022e90bc34b",
+    "url": "/femcare/static/js/main.b0e2b5ec.chunk.js"
+  },
+  {
+    "revision": "b9649f3c2f37302ac767",
+    "url": "/femcare/static/js/runtime-main.a46a269b.js"
+  },
+  {
+    "revision": "eac80561feaf33a06f0615ec4cc50248",
+    "url": "/femcare/static/media/Ellipse1.eac80561.png"
+  },
+  {
+    "revision": "ce0c0cea4b9ad7ac48d821af17f13e7f",
+    "url": "/femcare/static/media/Ellipse3.ce0c0cea.png"
+  },
+  {
+    "revision": "f1e933e0e3edac5ee1ed96917970146c",
+    "url": "/femcare/static/media/Intervencion1.f1e933e0.png"
+  },
+  {
+    "revision": "07000fa5b34cab2bd7d59ecfd2ecaf2c",
+    "url": "/femcare/static/media/Intervencion2.07000fa5.png"
+  },
+  {
+    "revision": "670f271ed0e2a92059bbc6f2c4e2f761",
+    "url": "/femcare/static/media/Intervencion3.670f271e.png"
+  },
+  {
+    "revision": "5f18cfaa50458912000c1edf2d4ae950",
+    "url": "/femcare/static/media/balon.5f18cfaa.png"
+  },
+  {
+    "revision": "d130c228c3cb57118bce7849feb8c248",
+    "url": "/femcare/static/media/bypass.d130c228.png"
+  },
+  {
+    "revision": "4675b4034f30c00dcd9d0817efd2adde",
+    "url": "/femcare/static/media/image.4675b403.jpeg"
+  },
+  {
+    "revision": "9ae72f27d00180d3e29174572e890619",
+    "url": "/femcare/static/media/logo.9ae72f27.png"
+  },
+  {
+    "revision": "0aaeabdbc619ea704f9e1a97fb7225d9",
+    "url": "/femcare/static/media/manga.0aaeabdb.png"
+  },
+  {
+    "revision": "5c4ad598c00147f46c9abf2704b931bc",
+    "url": "/femcare/static/media/topLogo.5c4ad598.jpeg"
+  }
+]);
